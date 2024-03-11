5b7f7cc111bbbae2b513c7b4d63989c28bc47341
marceloalejandrobencini <bencinimarcelo92@gmail.com>
2024-03-11 19:20:17 -0300

    con fetch

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -19,6 +19,16 @@ const App = () => {
 
   console.log('Todos:', todos);
 
+  function fetchTodos() {
+    fetch('https://playground.4geeks.com/apis/fake/todos/user/marcelobencini')
+      .then((response) => response.json())
+      .then((data) => {
+        setTodos(data);
+        console.log("data" ,data)
+      })
+      .catch((error) => console.log(error));
+  }
+
   function addTodo(title) {
     if (!title.trim()) {
       return; 
@@ -49,6 +59,31 @@ const App = () => {
     const updatedTodos = [];
     setTodos(updatedTodos);
   }
+  function syncTodos(updatedTodos) {
+    fetch('https://playground.4geeks.com/apis/fake/todos/user/marcelobencini', {
+      method: "PUT",
+      body: JSON.stringify(updatedTodos),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Todos synced successfully:", data);
+      })
+      .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  }
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <>
